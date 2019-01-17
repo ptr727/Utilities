5be520f0e6e57f88041ead2b9ce57fbab36435a2
Pieter Viljoen <ptr727@users.noreply.github.com>
2019-01-17 10:47:06 -0800

    Adding unit tests

---
+++ UtilitiesTests/CommandLineTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace InsaneGenius.Utilities.Tests
+{
+    public class CommandLineTests
+    {
+        [Fact]
+        public void Null()
+        {
+            Assert.False(false);
+        }
+    }
+}
---
+++ UtilitiesTests/Iso6393Tests.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace InsaneGenius.Utilities.Tests
+{
+    public class Iso6393Tests
+    {
+        [Fact]
+        public void Create_CreateIso6393List()
+        {
+            // Create full list of languages
+            List<Iso6393> iso6393List = Iso6393.Create();
+            Assert.NotNull(iso6393List);
+        }
+
+        [Theory]
+        [InlineData("afr", "Afrikaans")]
+        [InlineData("af", "Afrikaans")]
+        public void FromString_GetLanguageFromString(string input, string output)
+        {
+            // Create full list of languages
+            List<Iso6393> iso6393List = Iso6393.Create();
+            Assert.NotNull(iso6393List);
+
+            // Find matching language
+            Iso6393 language = Iso6393.FromString(input, iso6393List);
+            Assert.Equal(language.RefName, output);
+        }
+    }
+}
