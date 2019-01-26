6b9447bc314889a79ad3ae6e9cada9d1fc256966
Pieter Viljoen <pieter@viljoen.com>
2019-01-26 09:56:53 -0800

    Add CombinePath()

--- UtilitiesTests/FileTests.cs
+++ UtilitiesTests/FileTests.cs
@@ -4,10 +4,16 @@ namespace InsaneGenius.Utilities.Tests
 {
     public class FileTests
     {
-        [Fact]
-        public void Null()
+        [Theory]
+        [InlineData(@"C:\Path One", @"Path Two", @"C:\Path One\Path Two")]
+        [InlineData(@"C:\Path One\", @"\Path Two", @"C:\Path One\Path Two")]
+        [InlineData(@"C:\Path One\", @"/Path Two", @"C:\Path One\Path Two")]
+        [InlineData(@"\\Server\Path One\", @"\Path Two", @"\\Server\Path One\Path Two")]
+        public void CombinePath_RemoveRoots(string path1, string path2, string output)
         {
-            Assert.False(false);
+            // Combine the path
+            string path = FileEx.CombinePath(path1, path2);
+            Assert.Equal(path, output);
         }
     }
 }
