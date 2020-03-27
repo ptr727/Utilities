829660ec1cd649a54c16638d9571dd9244213698
Pieter Viljoen <ptr727@users.noreply.github.com>
2020-03-27 10:29:43 -0700

    Add option to trace to console

--- Utilities/FileExOptions.cs
+++ Utilities/FileExOptions.cs
@@ -10,6 +10,7 @@
         public Signal Cancel { get; }
         public int FileRetryCount { get; set; } = 1;
         public int FileRetryWaitTime { get; set; } = 5;
+        public bool TraceToConsole { get; set; } = false;
         public bool WaitForCancelFileRetry()
         {
             return Cancel.WaitForSet(FileRetryWaitTime * 1000);
