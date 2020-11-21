2adfe8c3380a41de938cef5fc60812981825f7a5
Pieter Viljoen <ptr727@users.noreply.github.com>
2020-11-21 10:01:12 -0800

    file path only works on Windows

--- UtilitiesTests/FileTests.cs
+++ UtilitiesTests/FileTests.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using System.Runtime.InteropServices;
+using Xunit;
 
 namespace InsaneGenius.Utilities.Tests
 {
@@ -13,6 +14,10 @@ namespace InsaneGenius.Utilities.Tests
         [InlineData(@"\\server\Path One\Path Two\", @"..\Path Three", @"\\server\Path One\Path Three")]
         public void CombineAbsolutePath2(string path1, string path2, string output)
         {
+            // This only work on Windows
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                return;
+
             string path = FileEx.CombinePath(path1, path2);
             Assert.Equal(path, output);
         }
@@ -26,6 +31,10 @@ namespace InsaneGenius.Utilities.Tests
         [InlineData(@"\\server\Path One\Path Two\", @"..\Path Three", @"foo.txt", @"\\server\Path One\Path Three\foo.txt")]
         public void CombineAbsolutePath3(string path1, string path2, string path3, string output)
         {
+            // This only work on Windows
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                return;
+
             string path = FileEx.CombinePath(path1, path2, path3);
             Assert.Equal(path, output);
         }
