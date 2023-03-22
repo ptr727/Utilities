973d2631c31b864b0e61c6db0b2683de0da84a18
Pieter Viljoen <ptr727@users.noreply.github.com>
2023-03-22 13:53:06 -0700

    fix logic

--- UtilitiesTests/Iso6393Tests.cs
+++ UtilitiesTests/Iso6393Tests.cs
@@ -23,7 +23,7 @@ public class Iso6393Tests
     [InlineData("deu", "German")]
     [InlineData("de", "German")]
     [InlineData("yue", "Yue Chinese")]
-    public void FromString(string input, string output)
+    public void Succeed_From_String(string input, string output)
     {
         // Create full list of languages
         List<Iso6393> iso6393List = Iso6393.Create();
@@ -33,5 +33,17 @@ public class Iso6393Tests
         Iso6393 language = Iso6393.FromString(input, iso6393List);
         Assert.NotNull(language);
         Assert.Equal(language.RefName, output);
+    }
+
+    [InlineData("xxx")]
+    public void Failed_From_String(string input, string output) 
+    {
+        // Create full list of languages
+        List<Iso6393> iso6393List = Iso6393.Create();
+        Assert.NotNull(iso6393List);
+
+        // Fail to find matching language
+        Iso6393 language = Iso6393.FromString(input, iso6393List);
+        Assert.Null(language);
     }
 }
