d1c678e39c13e234215663958ec1fc3150c2c1a3
Pieter Viljoen <ptr727@users.noreply.github.com>
2023-03-22 08:43:22 -0700

    Test for dynamically created culture info, rider refactor

--- Utilities/LogOptions.cs
+++ Utilities/LogOptions.cs
@@ -1,19 +1,18 @@
 ﻿using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 
-namespace InsaneGenius.Utilities
-{
-    public static class LogOptions
-    {
-        // Create logger from log factory
-        public static ILogger CreateLogger(ILoggerFactory loggerFactory)
-        {
-            // Create a logger using the provided logger factory
-            Logger = loggerFactory.CreateLogger("InsaneGeniusUtilities");
-            return Logger;
-        }
+namespace InsaneGenius.Utilities;
 
-        // Default logger
-        public static ILogger Logger { get; set; } = NullLogger.Instance;
+public static class LogOptions
+{
+    // Create logger from log factory
+    public static ILogger CreateLogger(ILoggerFactory loggerFactory)
+    {
+        // Create a logger using the provided logger factory
+        Logger = loggerFactory.CreateLogger("InsaneGeniusUtilities");
+        return Logger;
     }
+
+    // Default logger
+    public static ILogger Logger { get; set; } = NullLogger.Instance;
 }
