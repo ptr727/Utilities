d942f801b6bf9f4b4d216e10db2a7482205430a1
Pieter Viljoen <ptr727@users.noreply.github.com>
2023-03-22 08:43:22 -0700

    Test for dynamically created culture info, rider refactor

--- Utilities/CommandLineEx.cs
+++ Utilities/CommandLineEx.cs
@@ -1,36 +1,35 @@
 ﻿using System;
 
-namespace InsaneGenius.Utilities
+namespace InsaneGenius.Utilities;
+
+public static class CommandLineEx
 {
-    public static class CommandLineEx
+    // https://stackoverflow.com/questions/298830/split-string-containing-command-line-parameters-into-string-in-c-sharp
+    public static string[] ParseArguments(string commandLine)
     {
-        // https://stackoverflow.com/questions/298830/split-string-containing-command-line-parameters-into-string-in-c-sharp
-        public static string[] ParseArguments(string commandLine)
+        if (commandLine == null)
+            throw new ArgumentNullException(nameof(commandLine));
+
+        char[] paramChars = commandLine.ToCharArray();
+        bool inQuote = false;
+        for (int index = 0; index < paramChars.Length; index++)
         {
-            if (commandLine == null)
-                throw new ArgumentNullException(nameof(commandLine));
-
-            char[] paramChars = commandLine.ToCharArray();
-            bool inQuote = false;
-            for (int index = 0; index < paramChars.Length; index++)
-            {
-                if (paramChars[index] == '"')
-                    inQuote = !inQuote;
-                if (!inQuote && paramChars[index] == ' ')
-                    paramChars[index] = '\n';
-            }
-            return new string(paramChars).Split('\n');
+            if (paramChars[index] == '"')
+                inQuote = !inQuote;
+            if (!inQuote && paramChars[index] == ' ')
+                paramChars[index] = '\n';
         }
+        return new string(paramChars).Split('\n');
+    }
 
-        public static string[] GetCommandLineArgs()
-        {
-            // Split the arguments
-            string[] args = ParseArguments(Environment.CommandLine);
+    public static string[] GetCommandLineArgs()
+    {
+        // Split the arguments
+        string[] args = ParseArguments(Environment.CommandLine);
 
-            // Strip the process path from the list of arguments
-            string[] argsEx = new string[args.Length - 1];
-            Array.Copy(args, 1, argsEx, 0, argsEx.Length);
-            return argsEx;
-        }
+        // Strip the process path from the list of arguments
+        string[] argsEx = new string[args.Length - 1];
+        Array.Copy(args, 1, argsEx, 0, argsEx.Length);
+        return argsEx;
     }
 }
--- Utilities/FileExOptions.cs
+++ Utilities/FileExOptions.cs
@@ -1,24 +1,23 @@
 ﻿using System.Threading;
 
-namespace InsaneGenius.Utilities
+namespace InsaneGenius.Utilities;
+
+public class FileExOptions
 {
-    public class FileExOptions
+    // Test only, do not execute changes
+    public bool TestNoModify { get; set; }
+
+    // Cancel waiting operations
+    public CancellationToken Cancel { get; set; } = CancellationToken.None;
+
+    // Number of retries
+    public int RetryCount { get; set; } = 1;
+
+    // Wait time between retries in seconds
+    public int RetryWaitTime { get; set; } = 5;
+
+    public bool RetryWaitForCancel()
     {
-        // Test only, do not execute changes
-        public bool TestNoModify { get; set; }
-
-        // Cancel waiting operations
-        public CancellationToken Cancel { get; set; } = CancellationToken.None;
-
-        // Number of retries
-        public int RetryCount { get; set; } = 1;
-
-        // Wait time between retries in seconds
-        public int RetryWaitTime { get; set; } = 5;
-
-        public bool RetryWaitForCancel()
-        {
-            return Cancel.WaitHandle.WaitOne(RetryWaitTime * 1000);
-        }
+        return Cancel.WaitHandle.WaitOne(RetryWaitTime * 1000);
     }
 }
